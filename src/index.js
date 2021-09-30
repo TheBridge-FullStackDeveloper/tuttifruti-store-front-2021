861d6d34e8d2992cc183bfcaba12f273221ecadc
Giorgio Grassini <giorgiograssini@gmail.com>
2021-09-30 20:52:47 +0200

    Conflict test (#2)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* All starts here */}
+    {/* All starts here by Giorgio */}
   </React.StrictMode>,
   document.getElementById('root')
 )
