3d104c16b1281440ddd00a0cc777a8bdf33d2414
Molpe <fran.mscr@gmail.com>
2021-09-30 16:56:52 +0200

    feat: initial commit and cra structure created

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* All starts here */}
+  </React.StrictMode>,
+  document.getElementById('root')
+)
