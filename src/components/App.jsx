0c1723dbbaad4ad5c32ec633c3e53a231e29150a
Giorgio Grassini <giorgiograssini@gmail.com>
2021-10-01 19:17:29 +0200

    feat: cd/ci

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const APP = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
---
+++ src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { cleanup, render } from "@testing-library/react";
+
+import App from './App';
+
+afterEach(cleanup);
+
+it("should render App", () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText('Hello world')).toBeTruthy();
+});
