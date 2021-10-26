34b43a9c1dbd5a8dfd17634031724737d1fdfa96
maidersonn <maidersonn@gmail.com>
2021-10-26 23:47:08 +0200

    feat:add input

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -1,14 +1,13 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Input } from "@chakra-ui/react";
 
-export default function Searchbar(){
-
-	return (
-		<Box
-		w={800}
-		borderWidth={3}
-		borderColor="pink.300"
-		textAlign="center">
-			This is the place for the searchbar
-		</Box>
-	)
+export default function Searchbar({ search, handleSearch }) {
+  return (
+    <Box w={800} borderWidth={3} borderColor="pink.300" textAlign="center">
+      <Input
+        placeholder="Write the product name"
+        value={search}
+        onChange={handleSearch}
+      />
+    </Box>
+  );
 }
