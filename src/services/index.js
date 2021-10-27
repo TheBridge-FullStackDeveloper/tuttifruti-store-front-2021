c295adb339d2b6e5120b71c06ca6b5440a75469e
maidersonn <maidersonn@gmail.com>
2021-10-27 22:17:16 +0200

    fix: environment variable name

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAll, getBySearch } from "./products";
 
 const client = axios.create({
-  baseURL: "https://tutti-frutti-store.herokuapp.com/",
+  baseURL: process.env.REACT_APP_BACK_URL,
   headers: {
     "Content-Type": "application/json",
   },
